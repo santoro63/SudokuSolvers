a2d2ffbb30af0a49d2994ebec993a1b486b9fc03
Alex Santoro <Alexandre.Santoro@target.com>
2016-02-01 11:34:30 -0800

    working Java solution

---
+++ Java/src/com/kwh/Main.java
@@ -0,0 +1,35 @@
+package com.kwh;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Puzzle initialPuzzle = getPuzzleFromFile(args[0]);
+	    List<Puzzle> currentPuzzles = new LinkedList<>();
+        currentPuzzles.add(initialPuzzle);
+        while (!currentPuzzles.get(0).solved()) {
+            List<Puzzle> nextPuzzles = new LinkedList<>();
+            for (Puzzle p: currentPuzzles) {
+                nextPuzzles.addAll( p.expand());
+            }
+            currentPuzzles = nextPuzzles;
+        }
+        System.out.println(currentPuzzles.get(0));
+    }
+
+    private static Puzzle getPuzzleFromFile(String arg) throws IOException {
+        List<String> lines = Files.readAllLines( Paths.get(arg));
+        StringBuilder b = new StringBuilder();
+        for (String line : lines) {
+            b.append(line.substring(0,9));
+        }
+        return new Puzzle(b.toString().toCharArray());
+    }
+}
---
+++ Java/src/com/kwh/Puzzle.java
@@ -0,0 +1,120 @@
+// Copyright(c) 2016 Target Corporation
+package com.kwh;
+
+import java.util.*;
+
+/**
+ * Created by alexandre.santoro@target.com.
+ */
+public class Puzzle {
+
+    private static int[] rowLabels = new int[81];
+    private static int[] colLabels = new int[81];
+    private static int[] grpLabels = new int[81];
+
+    private static Set<Character> tileSet = new HashSet<>();
+
+    static {
+        for (int i=0; i<81; i++) {
+            rowLabels[i] = i / 9;
+            colLabels[i] = i % 9;
+            grpLabels[i] = 3 * (rowLabels[i]/3) + (colLabels[i]/3);
+        }
+        tileSet.add('1');
+        tileSet.add('2');
+        tileSet.add('3');
+        tileSet.add('4');
+        tileSet.add('5');
+        tileSet.add('6');
+        tileSet.add('7');
+        tileSet.add('8');
+        tileSet.add('9');
+    }
+
+    private char[] thePuzzle;
+
+    public Puzzle(char[] chars) {
+        thePuzzle = chars;
+    }
+
+    /**
+     * defines if puzzle is solved
+     * @return
+     */
+    public boolean solved() {
+        for (char c: thePuzzle) {
+            if (c == '_') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Creates an expanded set of the Puzzle, or returns an empty
+     * list if no expansion is possible. Expansion means creating
+     * a set of up to nine new puzzles where the first empty tile
+     * ('_') is replaced by the values '1'...'9'. Only valid expansions
+     * are returned.
+     *
+     * @return
+     */
+    public List<? extends Puzzle> expand() {
+        int pos = firstEmptyPos();
+        Set<Character> feasibleChars = getFeasibleChars(pos);
+        List<Puzzle> expandedPuzzles = new LinkedList<>();
+        for (Character c: feasibleChars) {
+            expandedPuzzles.add ( new Puzzle( copyAndReplace(thePuzzle, pos, c)));
+        }
+        return expandedPuzzles;
+    }
+
+    public String toString() {
+        StringBuilder b = new StringBuilder();
+        for (int i=0; i<9; i++) {
+            for (int j=0; j<9; j++) {
+                b.append(thePuzzle[9*i+j]);
+            }
+            b.append('\n');git 
+        }
+        return b.toString();
+    }
+
+    private int firstEmptyPos() {
+        for (int i = 0; i<81; i++) {
+            if (thePuzzle[i] == '_') {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Gets the list of feasible tiles. This would be the set of chars '1' .. '9'
+     * less the ones that already appear in the group, column or row for that
+     * position.
+     *
+     * @param pos the position for which we want the group, row and column tiles
+     * @return a (potentially empty) set of available tiles.
+     */
+    private Set<Character> getFeasibleChars(int pos) {
+        Set<Character> candidates = new HashSet<>();
+        candidates.addAll(tileSet);
+        for (int i=0; i<81; i++) {
+            if ( (rowLabels[i] == rowLabels[pos])
+                    || (colLabels[i] == colLabels[pos])
+                    || (grpLabels[i] == grpLabels[pos])){
+                candidates.remove( thePuzzle[i]);
+            }
+        }
+        return candidates;
+    }
+
+    private char[] copyAndReplace(char[] thePuzzle, int pos, Character c) {
+        char[] newPuzzle = new char[81];
+        System.arraycopy(thePuzzle,0,newPuzzle,0,81);
+        newPuzzle[pos] = c;
+        return newPuzzle;
+    }
+
+}

00bca4fabf1dfc33c7acb1d80ec2968e3573f4c0
Alex Santoro <santoro63@yahoo.com>
2022-10-03 18:04:53 -0700

    Adds working code
    Though we still need to understand and sort out the whole
    lazy-sequence conversion issues.

--- Clojure/src/clojuresudoku/core.clj
+++ Clojure/src/clojuresudoku/core.clj
@@ -18,6 +18,9 @@
      :grp (+ (* 3 (quot row 3)) (quot col 3))
      :val val }))
 
+(defn- tile-values [tile-seq]
+  (map (fn [x] (x :val)) tile-seq))
+
 (defn load-puzzle
   [filename]
   "Creates a puzzle from the specified file"
@@ -28,24 +31,79 @@
    (vec)
    ))
 
-(defn solve-puzzle
-  [puzzle-stack]
-  "Solves the puzzle in the stack."
-  nil)
+(defn solved? [puzzle]
+  "True if a puzzle is solved (by which we mean complete)."
+  (every? (fn [x] (not (= x \_))) (tile-values puzzle)))
 
+(defn- related? [t1 t2]
+  "Returns true if the two tiles are related."
+  (or (= (t1 :row) (t2 :row))
+      (= (t1 :col) (t2 :col))
+      (= (t1 :grp) (t2 :grp))))
+
+(defn related-tiles [puzzle tile]
+  (filter (fn [x] (related? x tile)) puzzle))
+
+(defn candidates-for
+  [puzzle empty-tile]
+  "Return a list of all allowed values for the puzzle at the specified index."
+  (difference
+   ALLOWED_VALUES
+   (set    (tile-values (related-tiles puzzle empty-tile)))))
+
+(defn- first-empty
+  [puzzle]
+  "Returns first empty tile found in the puzzle"
+  (let [tile (first puzzle)]
+  (cond (nil? puzzle) nil
+        (= \_ (tile :val)) tile
+        :else (first-empty (rest puzzle)))))
+
+(defn new-puzzle [puzzle empty-tile c]
+  (let [idx (empty-tile :idx)]
+    (vec
+     (map (fn [x] (if (= idx (x :idx)) (make-tile idx c) x)) puzzle))))
+
+(defn new-puzzles
+  [puzzle]
+  "Create a list of new puzzles from PUZZLE or an empty list if no new puzzles are possible.
+The new puzzles are created by replacing the first empty tile with all viable candidates."
+  (let [empty-tile (first-empty puzzle)]
+    (vec
+     (map (fn [c] (new-puzzle puzzle empty-tile c)) (candidates-for puzzle empty-tile)))))
+
+(defn solve-puzzle-depth-first
+  [puzzle-stack]
+  "Does the actual walk-through and solving."
+  (let [current-puzzle (peek puzzle-stack)
+        stack          (pop puzzle-stack)]
+  (cond (nil? current-puzzle) nil
+        (solved? current-puzzle) current-puzzle
+        :else (solve-puzzle-depth-first (vec (concat stack (new-puzzles current-puzzle)))))))
+
+(defn solve-puzzle
+  [puzzle]
+  "Solves the puzzle or returns nil if no solution exists."
+  (solve-puzzle-depth-first [puzzle]))
+
+  
 (defn puzzle-to-string
   [puzzle]
   "Returns a string representation of the puzzle"
-  nil)
+  (->>
+   puzzle
+   (tile-values)
+   (partition 9)
+   (map (fn [x] (clojure.string/join "" x)))
+   (clojure.string/join "\n")
+  ))
 
-(defn puzzle-values
-  [ puzzle]
-  "Return a vector with the contents of the puzzle"
-  nil)
 
 (defn -main [& args]
   (->>
    (load-puzzle (first args))
    (solve-puzzle)
    (puzzle-to-string)
-   (print)))
+   (println)
+   ))
+  
--- Clojure/test/clojuresudoku/core_test.clj
+++ Clojure/test/clojuresudoku/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [clojuresudoku.core :refer :all]))
 
+
+;;---------------
+;; FIXTURES
+;;---------------
+
+(def puzzle1 (load-puzzle "test/resources/puzzle1.sudoku"))
+
 ;;---------------
 ;; AUX FUNCTIONS
 ;;---------------
@@ -15,12 +22,18 @@
        (<= 0 (tile :idx) 80)
        (contains? ALLOWED_VALUES (tile :val))))
 
+(defn valid-tile-seq?
+  [tile-seq]
+  "True if all tiles in the set have distinct values"
+  (let [values (map (fn [x] (x :val)) tile-seq)]
+    (= SOLUTION_VALUES (set values))))
+
 ;;--------
 ;; TESTS
 ;;--------
 
 (deftest make-tile-test
-
+  
   (testing "tile creation is successful."
     (is (= {:idx  0 :row 0 :col 0 :grp 0 :val \1} (make-tile 0 \1)))
     (is (= {:idx 31 :row 3 :col 4 :grp 4 :val \_} (make-tile 31 \_)))))
@@ -29,7 +42,7 @@
 (deftest load-puzzle-test
 
   (testing "load works with good puzzles"
-    (let [ testee (load-puzzle "test/resources/puzzle1.sudoku") ]
+    (let [testee puzzle1] ; we already loaded the puzzle as a fixture
       (is (= 81 (count testee)))
       (is (every? valid-tile? testee))
       (is (= {:idx  0 :row 0 :col 0 :grp 0 :val \_} (testee 0))) 
@@ -37,3 +50,16 @@
       ))
   )
 
+(deftest candidates-generation-test
+
+  (testing "generates appropriate list of candidates"
+    (is (= #{\5 \6} (candidates-for puzzle1 (puzzle1 0))))))
+
+(deftest solve-puzzle-test
+  (testing "actually solves puzzles"
+    (let [ testee (solve-puzzle (load-puzzle "test/resources/puzzle1.sudoku")) ]
+      (is (solved? testee))
+      (is (every? valid-tile? testee))
+      ))
+  )<
+            

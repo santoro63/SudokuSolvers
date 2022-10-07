951a89606c23a3d5f3326fd770cee1753543fc91
Alex Santoro <santoro63@yahoo.com>
2022-10-07 15:08:11 -0700

    Fixes bug in project definition
    Was still using the old name in parts
    and ommitting class generation.

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -1,9 +1,7 @@
-(defproject cljsolver "0.2.0-SNAPSHOT"
+(defproject clojuresudoku "0.2.0-SNAPSHOT"
   :description "A CLI sudoku puzzle solver."
   :url "http://github.com:/santoro63/SudokuSolvers"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
-  :main ^:skip-aot clojuresudoku.core
+  :main clojuresudoku.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})

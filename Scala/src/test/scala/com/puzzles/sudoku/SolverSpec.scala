fe1afa380640e4d1820e2721d2bccbf17e58e195
Alex Santoro <Alexandre.Santoro@target.com>
2022-11-16 13:50:54 -0800

    Fixes bad test

--- Scala/src/test/scala/com/puzzles/sudoku/SolverSpec.scala
+++ Scala/src/test/scala/com/puzzles/sudoku/SolverSpec.scala
@@ -1,6 +1,6 @@
 package com.puzzles.sudoku
 
-import com.puzzles.sudoku.Solver.{ALLOWED_VALUES, Puzzle}
+import com.puzzles.sudoku.Solver.{ALLOWED_VALUES, FILLED_VALUES, Puzzle}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -19,24 +19,23 @@ class SolverSpec extends AnyFlatSpec with Matchers {
     val puzzle = Solver.loadPuzzle("./puzzles/easy.sudoku")
     val solvedPuzzle = Solver.solve(puzzle).get
     solvedPuzzle.length shouldBe 81
-    puzzle.map(_.content).forall(Solver.FILLED_VALUES.contains(_)) shouldBe true
-    allRowsAreGood(puzzle) shouldBe true
-    allColsAreGood(puzzle) shouldBe true
-    allGrpsAreGood(puzzle) shouldBe true
+    allRowsAreGood(solvedPuzzle) shouldBe true
+    allColsAreGood(solvedPuzzle) shouldBe true
+    allGrpsAreGood(solvedPuzzle) shouldBe true
   }
 
   private def groupIsGood( tiles: List[Tile]): Boolean = {
-    (tiles.length == 9) && (tiles.map(_.content).toSet == ALLOWED_VALUES)
+    (tiles.length == 9) && (tiles.map(_.content).toSet == FILLED_VALUES)
   }
 
   private def allRowsAreGood(puzzle: Puzzle): Boolean = {
-    puzzle.groupBy(_.row).values.toList.forall(groupIsGood(_))
+    puzzle.groupBy(_.row).values.forall(groupIsGood(_))
   }
   private def allColsAreGood(puzzle: Puzzle): Boolean = {
-    puzzle.groupBy( _.col).values.toList.forall(groupIsGood(_))
+    puzzle.groupBy( _.col).values.forall(groupIsGood(_))
   }
   private def allGrpsAreGood(puzzle: Puzzle): Boolean = {
-    puzzle.groupBy(_.grp).values.toList.forall(groupIsGood(_))
+    puzzle.groupBy(_.grp).values.forall(groupIsGood(_))
   }
 
 
